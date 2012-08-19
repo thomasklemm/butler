4766372ad8e70f9e4cb8dc7cb5720c5130211385
Thomas Klemm <github@tklemm.eu>
2012-08-20 00:42:16 +0200

    Railtie experiments, did not work out yet

--- lib/butler/railtie.rb
+++ lib/butler/railtie.rb
@@ -1,32 +1,32 @@
-require 'rails/all'
-
 module Butler
   class Railtie < ::Rails::Railtie
-    puts 'Processing Railtie...'
-    # config.butler = ActiveSupport::OrderedOptions.new # enable namespaced configuration in Rails environments
-
     # # Config
-    # use_butler   = Rails.application.config.assets.use_butler || nil
-    # path         = Rails.application.config.paths['public'].first
+    # Rails.application.config.butler = ActiveSupport::OrderedOptions.new # enable namespaced configuration in Rails environments
+    # enable_butler = Rails.application.config.butler.enable_butler || nil
+    # path          = Rails.application.config.paths['public'].first
 
-    # header_rules = Rails.application.config.assets.header_rules || {}
-    # options      = { header_rules: header_rules }
+    # header_rules  = Rails.application.config.butler.header_rules || {}
+    # options       = { header_rules: header_rules }
 
-    initializer "butler.add_middleware" do |app|
-      app.middleware.use Static
-      # if use_butler
-      #   if defined? ActionDispatch::Static
-      #     if defined? Rack::Cache # will not work
-      #       app.config.middleware.delete ActionDispatch::Static
-      #       app.config.middleware.insert_before Rack::Cache, Butler::Static, path, options
-      #     else
-      #       app.config.middleware.swap ActionDispatch::Static, Butler::Static, path, options
-      #     end
-      #   else
-      #     app.config.middleware.use Butler::Static, path, options
-      #   end
-      # end
-    end
+    # # Initializer
+    # initializer "butler.add_middleware" do |app|
+    #   #
+    #   # Insert Butler Middleware
+    #   # while trying to reach a certain middleware order
+    #   #
+    #   if enable_butler
+    #     if defined? ActionDispatch::Static
+    #       if defined? Rack::Cache
+    #         app.middleware.delete ActionDispatch::Static
+    #         app.middleware.insert_before Rack::Cache, Butler::Static, path, options
+    #       else
+    #         app.middleware.swap ActionDispatch::Static, Butler::Static, path, options
+    #       end
+    #     else
+    #       app.middleware.use Butler::Static, path, options
+    #     end
+    #   end
+    # end
 
   end
 end
